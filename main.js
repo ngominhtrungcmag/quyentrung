a74e075a3b61b0b20242ba797fa8c4cc45e3f256
nmtrung <trungnm@hvttgroup.com>
2024-05-20 14:49:23 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,73 @@
+const parallax = document.getElementById("home-img-lg");
+const parallax1 = document.getElementById("parallax1");
+const parallax2 = document.getElementById("parallax2");
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionX = offset*(-0.3)-100 + "px";
+})
+
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    offset-=3100;
+    parallax1.style.backgroundPositionY = offset*(0.1) + "px";
+})
+
+window.addEventListener("scroll", function()
+{
+    let offset = window.pageYOffset;
+    offset-=4800;
+    parallax2.style.backgroundPositionY = offset*(-0.1) + "px";
+})
+
+function myFunction() {
+    document.getElementById("check").checked = false;
+  }
+
+
+function reveal() {
+var reveals = document.querySelectorAll(".reveal");
+  
+for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+  
+window.addEventListener("scroll", reveal);
+
+
+function updateTimer() {
+  future  = Date.parse("June 30, 2024 11:00:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer").innerHTML = `
+    <div>${d}<span>ngày</span></div>
+    <div>${h}<span>giờ</span></div>
+    <div>${m}<span>phút</span></div>
+    <div>${s}<span>giây</span></div>
+  `;
+
+}
+setInterval('updateTimer()', 1000 );
